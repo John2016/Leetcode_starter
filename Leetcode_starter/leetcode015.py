942654f23e4a2737a8fc526b2babbfcd7413f650
Yuan <lhlxyjly@gmail.com>
2019-10-21 13:17:57 +0800

    015 basic

--- Leetcode_starter/leetcode015.py
+++ Leetcode_starter/leetcode015.py
@@ -4,3 +4,30 @@
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+
+        res = []
+        ii = 0
+
+        for ii in range(0, len(nums) - 2, 1):
+            if nums[ii] > 0:
+                break
+
+            jj = ii + 1
+            kk = len(nums) - 1
+
+            while jj < kk:
+                sum = nums[ii] + nums[jj] + nums[kk]
+
+                if sum > 0:
+                    kk -= 1
+                    while nums[kk-1] == nums[kk] and jj < kk:
+                        kk -= 1
+                elif sum < 0:
+                    jj += 1
+                    while nums[jj + 1] == nums[jj] and jj < kk:
+                        jj += 1
+                else:
+                    res.append([nums[ii], nums[jj], nums[kk] ] )
+
+        return res
