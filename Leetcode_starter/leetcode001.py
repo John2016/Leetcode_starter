b6a1c8465227e1c56038036e0ad70f1110de6ca9
Yuan <lhlxyjly@gmail.com>
2019-10-22 23:11:20 +0800

    lalala

--- Leetcode_starter/leetcode001.py
+++ Leetcode_starter/leetcode001.py
@@ -1,9 +1,21 @@
 ## leetcode 1001
-## 两数之和
+## 两数之和，求数组中和为给定数的两个数
 ## 要点是熟练使用mao数据结构
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-    
+    ## def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
+        table = {}
         for ii,num in enumerate(nums):
+            xx = table.get(num)
+            if xx is not None:
+                return [xx, ii]
+            
+            table[target-num] = ii
+
+        return None
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.twoSum([3,3],6))
             
--- Leetcode_starter/leetcode036.py
+++ Leetcode_starter/leetcode036.py
@@ -4,10 +4,39 @@
 
 
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
+    ## def isValidSudoku(self, board: List[List[str]]) -> bool:
+    def isValidSudoku(self, board):
         # init three maps
-        
+        row_map = [ [False for _ in range(10)] for _ in range(9)]
+        col_map = [ [False for _ in range(10)] for _ in range(9)]
+        box_map = [ [False for _ in range(10)] for _ in range(9)]
 
         # validate
         for ii in range(9):
             for jj in range(9):
+                if board[ii][jj] != '.':
+                    num = int(board[ii][jj])
+                    box_id = ii//3 * 3 + jj//3
+                    if (row_map[ii][num] and col_map[jj][num] and box_map[box_id][num]):
+                        return False
+                    else:
+                        row_map[ii][num] == True
+                        col_map[jj][num] == True
+                        box_map[box_id][num] = True
+        return True
+
+if __name__ == '__main__':
+    s = Solution()
+    board = [
+            ["8","3",".",".","7",".",".",".","."],
+            ["6",".",".","1","9","5",".",".","."],
+            [".","9","8",".",".",".",".","6","."],
+            ["8",".",".",".","6",".",".",".","3"],
+            ["4",".",".","8",".","3",".",".","1"],
+            ["7",".",".",".","2",".",".",".","6"],
+            [".","6",".",".",".",".","2","8","."],
+            [".",".",".","4","1","9",".",".","5"],
+            [".",".",".",".","8",".",".","7","9"]
+            ]
+
+    print(s.isValidSudoku(board))
