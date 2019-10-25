07c0f7abe159c90dc4eb49229fe721cffcf5f899
Yuan <lhlxyjly@gmail.com>
2019-10-25 22:51:53 +0800

    normal upadate

--- Leetcode_starter/leetcode003.py
+++ Leetcode_starter/leetcode003.py
@@ -1,3 +1,24 @@
 ## leetcode 1003
 ## 找出字符串种最长的无重复子串
 
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+
+        bool_array = [-1 for i in range(256)]
+        last_occur_pos = -1
+        res = 0
+        
+        for ii, ch in enumerate(s):
+            if bool_array[ord(ch)] != -1 and last_occur_pos < bool_array[ord(ch)]:
+                last_occur_pos = bool_array[ord(ch)]
+            if (ii - last_occur_pos > res):
+                res = ii - last_occur_pos
+            bool_array[ord(ch)] = ii
+
+        return res
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.lengthOfLongestSubstring('abba'))
--- Leetcode_starter/leetcode037.py
+++ Leetcode_starter/leetcode037.py
@@ -2,3 +2,19 @@
 ## 解数独
 ## 基于第36题，加入回溯
 
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        def is_available():
+
+
+        def insert_num():
+
+
+        def remove_num():
+
+        
+        def add_next():
