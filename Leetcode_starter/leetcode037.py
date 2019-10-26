21f7f5c42d95763ed07b4be0602c16a2520fe804
Yuan <lhlxyjly@gmail.com>
2019-10-26 22:56:37 +0800

    tmp update

--- Leetcode_starter/leetcode037.py
+++ Leetcode_starter/leetcode037.py
@@ -8,13 +8,23 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
 
-        def is_available():
+        def is_available(row, col, box, num):
+            # box_index = (row // 3) * 3 + col//3
+            return not (rol_map[row][num] or col_map[col][num] or box_map[box_index][num])
 
-
-        def insert_num():
+        def insert_num(row, col, box, num):
+            rol_map[row][num] = True
+            col_map[col][num] = True
+            box_map[box][num] = True
 
 
         def remove_num():
-
+            rol_map[row][num] = False
+            col_map[col][num] = False
+            box_map[box][num] = False
         
         def add_next():
+
+
+        def backtack(rol, col):
+
