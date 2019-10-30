5096809dd07aaa8ecb2db6755be85c571574c28e
Yuan <lhlxyjly@gmail.com>
2019-10-30 12:42:55 +0800

    tmp update

--- Leetcode_starter/leetcode004.py
+++ Leetcode_starter/leetcode004.py
@@ -4,4 +4,9 @@
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        num_1 = nums1.len()
+        num_2 = nums2.len()
+
+        left = (num_1 + num_2 + 1) / 2
+        right = (num_1 + num_2 + 2) / 2
         
